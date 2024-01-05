d442ee47f83bfcda90c070a0a2edce0ddc066920
simaa <simaa@ahmeds-mbp.lan>
2024-01-04 23:19:40 -0500

    ABC

---
+++ src/s/DemoS.java
@@ -0,0 +1,10 @@
+package s;
+
+public class DemoS {
+	public static void main(String[] args) {
+		System.out.println("Tuesday");
+		System.out.println("Sprint 3 work..");
+		System.out.println("Sprint 4 work..");
+	}
+
+}
